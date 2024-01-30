47f5b97c2c9e503b00efa9e09885721afa19cd87
hercules1997 <hercules.chaves.andrade@gmail.com>
2024-01-29 23:29:27 -0300

    Instalando cors

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import express, { json } from 'express'
 import { routes } from './routes'
 import { setupMongo } from './database'
 import { errorHandle } from './middlewares/error-handle.middleware'
-
+import cors from 'cors'
 setupMongo().then(() => {
   const app = express()
 
+  app.use(cors())
   app.use(json())
   app.use(routes)
   app.use(errorHandle)
